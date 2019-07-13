34805e36278dc30810bb183f58075e814746241f
Mr-stark07 <divyaprakash_5@hotmail.com>
2019-07-13 20:11:26 +0530

    skeleton

---
+++ src/test/java/com/blogSpot/blogSpot/BlogSpotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogSpot.blogSpot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogSpotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
